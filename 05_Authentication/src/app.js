f5aaec4ef07c4e873837e8a7a7ba71f7367e05c2
Sachin Raghuwanshi <raghuwanshisachin887@gmail.com>
2026-08-31 15:48:47 +0530

    Add JWT cookie authentication

--- 05_Authentication/src/app.js
+++ 05_Authentication/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const authRoutes = require("./routes/auth.routes")
 const cookieParser = require('cookie-parser')
+const postRoutes = require('../src/routes/post.routes')
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes)
-
+app.use("/api/post", postRoutes)
 
 module.exports = app
---
+++ 05_Authentication/src/routes/post.routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const jwt = require('jsonwebtoken')
+const user = require('../models/user.model');
+const userModel = require('../models/user.model');
+const router = express.Router();
+
+router.post("/create",async(req,res)=>{
+
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+        const user = await userModel.findOne({
+            _id: decoded.id
+        })
+
+        console.log(user)
+
+    }catch(err){
+        return res.status(401).json({
+            message:"Invalid token"
+        })
+    }
+    res.send("Post created successfully")
+})
+
+module.exports = router;
